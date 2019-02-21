974be448a6eac4d0588fbb0b29da5cacbabced72
Camille Chapleau <camille.chapleau@hotmail.fr>
2019-02-21 16:19:22 +0100

    halfway through the exercice

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from  './FoodBox.js';
+import Form from './Form.js';
 
 class App extends Component {
+  constructor() {
+    super(); //this runs React Component's constructor
+    this.state = {
+      foods: foods
+    };
+  }
+
+  addFood = () => {
+
+
+  };
+  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>IronNutrition</h1>
+      <div>
+    <input type="text" className="input" placeholder="Search..." />
+    <button onClick={ () => this.addFood ()} >Add Food</button>
+</div>
+        {this.state.foods.map((food, index) => {
+          return <FoodBox key={index} img={food.image} name={food.name} calories={food.calories}/>;
+        })}
+
+        <FoodBox />
       </div>
     );
   }
 }
 
 export default App;
+
+
---
+++ starter-code/src/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import { props } from 'bluebird';
+
+class Form extends Component {
+
+    constructor() {
+      super(); //this runs React Component's constructor
+
+      };
+    }
+
+    export default Form;
